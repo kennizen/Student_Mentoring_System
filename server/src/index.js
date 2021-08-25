d215eaf8f96e1d260256819ed2c42b3044ee8660
kennizen <prachurjyagogoi123@gmail.com>
2021-08-25 23:35:46 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 // routes
-const adminRouter = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 
 // mongoose config
 require("./config/mongoose");
@@ -15,7 +16,8 @@ app.use(cors());
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-app.use("/admin", adminRouter);
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
+
+app.use("/admin", adminRoutes);
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
-
